0d9e261b4d92244cd7af8f60ceb155cf2f473328
comsir <1573282335@qq.com>
2019-06-25 15:53:56 +0800

    k_treePath2

--- song/tree/com/k_treePath2.java
+++ song/tree/com/k_treePath2.java
@@ -21,8 +21,12 @@ public class k_treePath2 {
         });
         return res;
     }
+    /*
+    *输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+    * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+    * */
     public static void helper(TreeNode root,int target,ArrayList<Integer> cur,ArrayList<ArrayList<Integer>> res){
-        if (root==null) return;
+        if (root==null) return;  //这个是跳出来的操作，然后用cur.remove往上一层跳
         int value=root.val;
         cur.add(value);
 
@@ -38,25 +42,23 @@ public class k_treePath2 {
 
     public static void main(String[] args) {
         TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(9);
 
-        TreeNode node4 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(2);
         TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(4);
+
 
         node1.left = node2;
         node1.right = node3;
         node2.left = node4;
         node2.right = node5;
-        node3.left = node6;
-        node3.right = node7;
 
-        ArrayList<ArrayList<Integer>> a = FindPath(node1, 8);
+
+        ArrayList<ArrayList<Integer>> a = FindPath(node1, 10);
         System.out.println(a.size());
 
-        ArrayList<Integer> b = a.get(0);
+        ArrayList<Integer> b = a.get(1);
         for (Integer integer : b) {
             System.out.println(integer);
         }
