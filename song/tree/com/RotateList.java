7be8b093295c25c8bec5f2765b5bfd5db6083305
comsir <1573282335@qq.com>
2019-07-03 11:23:24 +0800

    rotateList

---
+++ song/tree/com/RotateList.java
@@ -0,0 +1,46 @@
+package song.tree.com;
+
+public class RotateList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode listNode = rotateRight(node1, 2);
+        System.out.println(listNode);
+    }
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head==null||head.next==null) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode fast=dummy,slow=dummy;
+
+        int i;
+        for (i=0;fast.next!=null;i++)//Get the total length
+            fast=fast.next;
+
+        //精华都在这一步
+        //重复出现的取余去重
+
+        for (int j=i-k%i;j>0;j--) //Get the i-n%i th node
+            slow=slow.next;
+
+        fast.next=dummy.next; //Do the rotation
+        dummy.next=slow.next;
+        slow.next=null;
+
+        return dummy.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }}
+
+
+}
