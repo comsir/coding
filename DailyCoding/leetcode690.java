375831ce4fa6e1d74a23d846398bc86a1b401d87
comsir <1573282335@qq.com>
2020-02-04 23:47:48 +0800

    0204

--- DailyCoding/leetcode690.java
+++ DailyCoding/leetcode690.java
@@ -6,14 +6,10 @@ import java.util.Map;
 
 public class leetcode690 {
 
-    // Employee info
     class Employee {
-        // It's the unique id of each node;
-        // unique id of this employee
+
         public int id;
-        // the importance value of this employee
         public int importance;
-        // the id of direct subordinates
         public List<Integer> subordinates;
     };
     public static void main(String[] args) {
@@ -30,12 +26,12 @@ public class leetcode690 {
         return dfs(id);
     }
 
-    public int dfs(int eid) {
-        Employee employee = emap.get(eid);
-        int ans = employee.importance;
+    public int dfs(int eid) { //一个ID进来
+        Employee employee = emap.get(eid);  //通过这个ID得到这个类
+        int ans = employee.importance;  //得到这个员工的重要度
         for (Integer subid:
              employee.subordinates) {
-            ans += dfs(subid);
+            ans += dfs(subid);  //深度搜索
         }
         return ans;
     }
